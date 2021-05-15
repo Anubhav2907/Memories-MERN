455af8b4d25bf5f0fd0f71c97f9df3ef163c0c50
Anubhav2907 <76243969+Anubhav2907@users.noreply.github.com>
2021-05-16 02:56:08 +0530

    adding action creator

---
+++ client/memories/src/reducers/posts.js
@@ -0,0 +1,11 @@
+const postReducer = (posts = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return posts;
+    case "CREATE":
+      return posts;
+    default:
+      return posts;
+  }
+};
+export default postReducer;
